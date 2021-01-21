339cb304175570bf444a4077a9514e809b6bd303
Miguel-Venegas <venegas.a.miguel@gmail.com>
2021-01-21 14:48:08 -0800

    learning async and await

--- app.js
+++ app.js
@@ -21,3 +21,20 @@ async function getPlanets(){
 	console.log(res.data);
 }
 
+// what happens when, if nothing is returned? The above getPlanets function does nothing to show that case. Here is a way to make sense of an error. Just add catch at the end of the invoked async function like so:
+
+getPlanets().catch((err) => {
+	console.log(err);
+	console.log('in catch!!!')
+})
+
+// there is another way to do this. You can add a 'try and catch block' like so:
+
+async function getPlanets(){
+	try {
+		const response = await axios.get('https://swapi.co/api/planets/');
+		console.log(res.data);
+	} catch (err) {
+		console.log('in catch', err)
+	}
+}
